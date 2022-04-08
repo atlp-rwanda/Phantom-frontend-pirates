97de1904c398607dfb520f3b39915ac90e24deef
christian <habinezac15@gmail.com>
2022-04-08 14:10:19 +0200

    ft: landing-page added
    ft: requested changes added
    
    ft: changes requested added
    
    language naming changed
    
    ft: styling language selector
    
    ft: making select language more visible
    
    ft: inputs catched and return responses

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAsyncViewBus } from '../features/viewBus/ViewBusSlice';
+import { getViewBus } from '../features/viewBus/ViewBusSlice';
+import ViewBus from './ViewBus';
+import { useNavigate } from 'react-router-dom';
+
+const SearchBox = () => {
+  const [source, setSource] = useState('');
+  const [destination, setDestination] = useState('');
+
+  const dispatch = useDispatch();
+  const viewBusData = useSelector(getViewBus);
+  const navigate = useNavigate();
+
+  viewBusData.routeobject ? (
+    viewBusData.routeobject.map(
+      (bus, index) => (
+        (<ViewBus key={index} data={bus} />), navigate('/viewBus')
+      )
+    )
+  ) : (
+    <div>
+      <h3>{viewBusData.Error}</h3>
+    </div>
+  );
+
+  const busData = {
+    source,
+    destination,
+  };
+
+  const handleClick = () => {
+    dispatch(fetchAsyncViewBus(busData));
+  };
+
+  return (
+    <div
+      className="absolute w-[80%] h-[400px] max-w-[400px] sm:bottom-[5%] bottom-[10%] bg-[#000000] bg-opacity-[50%]
+            border border-slate-300 rounded-xl shadow-xl sm:max-w-[600px] md:max-w-[70%] md:h-[250px] md:bottom-[13%]"
+    >
+      <div className="flex md:justify-start justify-center">
+        <p className="relative text-white  pt-4 md:pl-[9%] mb-[-12px] font-bold italic">
+          How far is the bus
+        </p>
+      </div>
+      <div className="grid grid-cols-1 gap-y-5 place-items-center md:flex md:flex-wrap md:justify-center md:gap-5 pt-10 italic">
+        <input
+          className="md:w-[40%] px-5 h-[32px] rounded-[5px] italic bg-white focus:bg-opacity-[50%] focus:outline-none focus:text-white focus:text-semibold"
+          s
+          type="text"
+          placeholder="source"
+          onChange={(e) => setSource(e.target.value)}
+        />
+        <input
+          className="md:w-[40%] px-5 h-[32px] rounded-[5px] italic bg-white focus:bg-opacity-[50%] focus:outline-none focus:text-white focus:text-semibold"
+          s
+          type="text"
+          placeholder="destination"
+          onChange={(e) => setDestination(e.target.value)}
+        />
+        <input
+          className="md:w-[40%] px-5 h-[32px] rounded-[5px] italic bg-white focus:bg-opacity-[50%] focus:outline-none focus:text-white focus:text-semibold"
+          s
+          type="text"
+          placeholder="your location"
+        />
+        <input
+          className="md:w-[40%] px-5 h-[32px] rounded-[5px] italic bg-white focus:bg-opacity-[50%] focus:outline-none focus:text-white focus:text-semibold"
+          s
+          type="text"
+          placeholder="other source (optional)"
+        />
+      </div>
+      <div className="pt-5 flex md:justify-end justify-center md:pr-[9%]">
+        <button
+          className="bg-[#FFC107] border-none rounded-[5px] h-[32px] w-[40%] md:w-[16%] italic text-black font-bold hover:bg-opacity-[80%]"
+          onClick={handleClick}
+        >
+          Find Bus
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBox;
--- src/container/LandingPage.jsx
+++ src/container/LandingPage.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import Counter from "../components/counter";
-const LandingPage = () => {
-  return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-            Hello Phantom, This is our Landing Page
-        </h1>
-        <div className='box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2'>
-        <Counter />
-        </div>
-    </div>
-)}
+import React, { useState } from 'react';
+import { Navbar, SearchBox } from '../components';
 
-export default LandingPage
+const LandingPage = () => {
+  const myStyle = {
+    backgroundImage:
+      "url('https://res.cloudinary.com/dmgfxu4fg/image/upload/v1649259188/WhatsApp_Image_2022-04-06_at_5.24.21_PM_mqnrhn.jpg')",
+  };
+
+  return (
+    <div className="md:w-full md:h-screen h-fit">
+      <div
+        style={myStyle}
+        className="w-full h-full bg-no-repeat bg-cover bg-[#C4C4C4] absolute"
+      >
+        <Navbar />
+      </div>
+      <div className="max-w-[1240px] mx-auto text-black relative">
+        <div className="py-24 md:px-[7%] px-7 md:py-36">
+          <h1 className="font-bold text-white text-2xl sm:text-4xl md:text-5xl">
+            MAKE YOUR TRANSPORT EASIER WITH PHANTOM
+          </h1>
+          <p className="pt-4 text-xl font-medium md:text-4xl sm:text-3xl text-white">
+            At <span className="text-yellow-400">PHANTOM</span> we value your
+            time.
+          </p>
+          <p className="text-xxl font-medium md:text-4xl sm:text-3xl text-white">
+            Track how far is your bus and save you Time.
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <SearchBox />
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
