f5bd2858efbe6ccf18f0eb98992518b34cf1eca7
christian <habinezac15@gmail.com>
2022-04-08 14:10:19 +0200

    ft: landing-page added
    ft: requested changes added
    
    ft: changes requested added
    
    language naming changed
    
    ft: styling language selector
    
    ft: making select language more visible
    
    ft: inputs catched and return responses
    
    ft: searching box updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render (
-<Router>
+ReactDOM.render(
+  <Router>
     <App />
-</Router>, 
-document.getElementById('root'));
+  </Router>,
+  document.getElementById('root')
+);
