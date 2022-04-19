2b9eb594fa74ab2685661a936eb32cac66b2d9fc
pacifique <pacifiquenyiringango1998@gmail.com>
2022-04-19 17:03:06 +0200

    login to phantom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './container/LandingPage';
 import { NotFound } from './components';
 import ViewBus from './components/ViewBus';
+import Login from './container/Login';
+import Notifications from './container/Notifications';
 
 const App = () => {
   return (
-    <div>
-      <React.StrictMode>
+    <>
+    <Router>
+      <div className='container'>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/viewBus" element={<ViewBus />} />
-          <Route path="/*" element={<NotFound />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/viewBus" element={<ViewBus />} />
+            <Route path="/*" element={<NotFound />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/notification' element={<Notifications />} />
         </Routes>
-      </React.StrictMode>
-    </div>
+      </div>
+    </Router>
+   </>
   );
 };
 
