e973469c8f72ba0f8bad9c13689308d75396281d
pacifique <pacifiquenyiringango1998@gmail.com>
2022-04-24 16:40:50 +0200

    login to phantom
    ch-multiple language
    
    bug-fix: support multiple languages
    
    bug-fix-google-button

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import viewBusReducer from "../features/viewBus/ViewBusSlice";
-import authReducer from "../features/auth/authSlice";
-import routeReducer from "../features/Route/routeSlice";
-import EmployeesReducer from "../features/Employees/EmployeeSlice";
-import RoleReducer from "../features/Roles/RoleSlice";
-import CompanyReducer from "../features/company/companySlice";
+import { configureStore } from '@reduxjs/toolkit';
+import viewBusReducer from '../features/viewBus/ViewBusSlice';
+import authReducer from '../features/auth/authSlice'
+
 export const store = configureStore({
   reducer: {
     viewSearchedBuses: viewBusReducer,
-    auth: authReducer,
-    routes: routeReducer,
-    employees: EmployeesReducer,
-    roles: RoleReducer,
-    companies: CompanyReducer
+    auth: authReducer
   },
 });
