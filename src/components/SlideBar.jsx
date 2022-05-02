0ab75ae5b1e48d4c7271c806087587ec5173a483
Julesbi <hijules10@gmail.com>
2022-05-02 15:28:52 +0200

    sidebar updated

--- src/components/SlideBar.jsx
+++ src/components/SlideBar.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import logo from "../images/logo.png";
 
-function RegisterSidebar() {
+function SlideBar() {
   return (
     <div
       id="Main"
@@ -11,59 +10,50 @@ function RegisterSidebar() {
       <div>
         <img src={logo} alt="/" className="w-66 pl-10" />
       </div>
-      <div className="xl:mt-40 flex flex-col justify-start items-start  px-4 w-full space-y-3 pb-5">
-        <button className="focus:outline-none flex justify-start items-center space-x-6 hover:text-white bg-white border-none hover:bg-cyan-700 text-gray-600 rounded  py-3 pl-4  w-full ">
-          <div className="flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
-              />
-            </svg>
-            <p className="text-base leading-4 ml-2 ">Permissions
-            </p>
-          </div>
+      <div className="xl:mt-6 flex flex-col justify-start items-start  px-4 w-full space-y-3 pb-5 ">
+        <div className=" relative focus:outline-none flex jusitfy-start w-full   text-gray-800 rounded  items-center border-gray-300 focus:border-gray-400 border  "></div>
+        <button className="focus:outline-none flex jusitfy-start hover:text-white bg-white border-none hover:bg-cyan-700 text-gray-600 rounded py-3 pl-4 items-center space-x-6 w-full ">
+          <img
+            src="https://res.cloudinary.com/basha18/image/upload/v1652190001/home_w0j6n7.svg"
+            alt="/"
+            className="w-8"
+          />
+          <p className="text-base leading-4 ">Dashboard</p>
         </button>
-
+        <button className="focus:outline-none flex jusitfy-start hover:text-white bg-white border-none hover:bg-cyan-700 text-gray-600 rounded py-3 pl-4  items-center w-full  space-x-6">
+          <img
+            src="https://res.cloudinary.com/basha18/image/upload/v1652190001/bus-alt_eijyat.svg"
+            alt="/"
+            className="w-8"
+          />
+          <p className="text-base leading-4 ">Buses</p>
+        </button>
+        <button className="focus:outline-none flex justify-start items-center space-x-6 hover:text-white bg-white border-none hover:bg-cyan-700 text-gray-600 rounded  py-3 pl-4  w-full ">
+          <img
+            src="https://res.cloudinary.com/basha18/image/upload/v1652190588/user-group_b96btk.svg"
+            alt="/"
+            className="w-8"
+          />
+          <p className="text-base leading-4 ">Employees</p>
+        </button>
         <button className="flex justify-start items-center space-x-6 hover:text-white focus:outline-none focus:bg-indigo-700 focus:text-white bg-white border-none hover:bg-cyan-700 text-gray-600 rounded py-3 pl-4  w-full ">
-          <div className="flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-            <p className="text-base leading-4 ml-2 ">
-              <Link to="/employees">Employees</Link>
-            </p>
-          </div>
+          <img
+            src="https://res.cloudinary.com/basha18/image/upload/v1652190106/road_u6zkmx.svg"
+            alt="/"
+            className="w-8"
+          />
+          <p className="text-base leading-4  ">Routes</p>
         </button>
       </div>
       <div className="w-full px-4">
         <hr className=" border-gray-100 w-full" />
       </div>
 
-      <div className="w-full ">
-        <hr className=" border-black w-full" />
+      <div className="w-full px-4">
+        <hr className=" border-gray-100 w-full" />
       </div>
 
-      <div className="mt-36 flex  bg-white justify-start space-x-2 items-center h-full py-4 px-3.5 w-full ">
+      <div className="mt-36 flex  bg-white justify-start space-x-2 items-center h-full py-4 px-3.5    w-full  ">
         <div>
           <img src="https://i.ibb.co/fxrbS6p/Ellipse-2-2.png" alt="avatar" />
         </div>
@@ -81,7 +71,7 @@ function RegisterSidebar() {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -104,4 +94,4 @@ function RegisterSidebar() {
   );
 }
 
-export default RegisterSidebar;
+export default SlideBar;
---
+++ src/container/Employees.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import Modal from "../components/AddEmployeeModal";
+import SideBar from "../components/SlideBar";
+import EmployeesTable from "../components/EmployeesTable";
+
+function Index() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    
+  }, [user, navigate, dispatch]);
+
+  return (
+    <div className="flex flex-no-wrap">
+      <div className="w-64 absolute sm:relative bg-indigo-900 shadow md:h-full flex-col justify-between hidden sm:flex">
+        <SideBar />
+      </div>
+
+      <div className="container mx-auto py-10 bg-gray-100 h-auto md:w-4/5 w-11/12 px-6">
+        <div className="flex justify-around">
+          <div>
+            <h2 className="text-lg text-cyan-700 font-bold">Admin Pannel</h2>
+          </div>
+          <div className="flex border-2 bg-white">
+            <button className="flex items-center justify-center px-4 bg-white border-none">
+              <svg
+                className="w-6 h-6 text-gray-600"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path>
+              </svg>
+            </button>
+            <input
+              type="text"
+              className="px-4 py-2 w-80"
+              placeholder="Search..."
+            />
+          </div>
+        </div>
+        <div className="p-6 w-full bg-white mt-12 place-content-center rounded-lg border border-gray-200 shadow-md">
+          <div className="place-content-center">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                <div className="overflow-hidden grid place-items-center">
+                  <div className="w-10/12 flex justify-between">
+                    <div>
+                      <h2>Employees List</h2>
+                    </div>
+                    <div>
+                      <div className="flex flex-col items-center justify-center">
+                        <Modal />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="relative focus:outline-none flex jusitfy-start w-full mt-6 mb-4   text-gray-800 rounded  items-center border-cyan-700 focus:border-gray-400 border-b-8  "></div>
+                  <EmployeesTable />
+                </div>
+                <div className=" w-11/12 flex justify-end">
+                  <div>
+                    <button className="flex jusitfy-center hover:text-white bg-white hover:bg-cyan-700 text-gray-600 rounded p-3 mt-6 items-center">
+                      <p className="text-base leading-4 ">More</p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Index;
