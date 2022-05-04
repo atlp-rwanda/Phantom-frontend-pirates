aac3cdf08b4e58c14217bf0d165e61afb34923cf
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-05-04 16:17:19 +0200

    bus simulation
    bus simulation
    
    bus-with-sockets

---
+++ src/components/LeafMap.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+import Routing from './Routing';
+
+const center = { lat: -1.9431208545984047, lng: 30.057279438873277 }
+
+const busStopData = {
+    'Downtown Bus park': {
+        name: 'Downtown Bus park',
+        lat: -1.9431208545984047,
+        lng: 30.057279438873277
+    },
+    'Nyabugogo Bus park': {
+        name: 'Nyabugogo Bus park',
+        lat: -1.9405011634756917,
+        lng: 30.04490505421663
+    }
+    ,
+    'Kacyiru Bus Park': {
+        name: 'Kacyiru Bus Park',
+        lat: -1.9363377719102153,
+        lng: 30.081289227231704
+    },
+    'Remera Bus park': {
+        name: 'Remera Bus park',
+        lat: -1.958411956926637,
+        lng: 30.119027896544978
+    }
+}
+
+const LeafMap = ({ source, destination, start }) => {
+    const [map, setMap] = useState(null);
+    const [pointA, setPointA] = useState('');
+    const [pointB, setPointB] = useState('');
+    useEffect(() => {
+
+      setPointA(busStopData[source])
+      setPointB(busStopData[destination])
+
+    }, [source, destination])
+
+    return (
+        <MapContainer
+            center={center}
+            zoom={15}
+            style={{ width: "100%", height: "100vh" }}
+            whenCreated={setMap}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Routing 
+                pointA={pointA} 
+                pointB={pointB}
+                start={start}
+            />
+        </MapContainer>
+    )
+}
+
+export default LeafMap; 
---
+++ src/utils/Animated.marker.js
@@ -0,0 +1,152 @@
+let counter = 0, resume = 0;
+
+L.AnimatedMarker = L.Marker.extend({
+  options: {
+    isStarted: false,
+    isStopped: false,
+    // meters
+    distance: 200,
+    // ms
+    interval: 1000,
+    // animate on add?
+    autoStart: true,
+    // callback onend
+    onEnd: function(){},
+    clickable: false
+  },
+
+  initialize: function (latlngs, options) {
+    this.setLine(latlngs);
+    L.Marker.prototype.initialize.call(this, latlngs[resume], options);
+  },
+
+  // Breaks the line up into tiny chunks (see options) ONLY if CSS3 animations
+  // are not supported.
+  _chunk: function(latlngs) {
+    var i,
+        len = latlngs.length,
+        chunkedLatLngs = [];
+
+    for (i=1;i<len;i++) {
+      var cur = latlngs[i-1],
+          next = latlngs[i],
+          dist = cur.distanceTo(next),
+          factor = this.options.distance / dist,
+          dLat = factor * (next.lat - cur.lat),
+          dLng = factor * (next.lng - cur.lng);
+
+      if (dist > this.options.distance) {
+        while (dist > this.options.distance) {
+          cur = new L.LatLng(cur.lat + dLat, cur.lng + dLng);
+          dist = cur.distanceTo(next);
+          chunkedLatLngs.push(cur);
+        }
+      } else {
+        chunkedLatLngs.push(cur);
+      }
+    }
+    chunkedLatLngs.push(latlngs[len-1]);
+
+    return chunkedLatLngs;
+  },
+
+  onAdd: function (map) {
+    L.Marker.prototype.onAdd.call(this, map);
+
+    // Start animating when added to the map
+   if (this.options.autoStart) {
+      if (!this.options.isStarted) {
+        this.start();
+        this.options.isStarted = true;
+      }
+    }
+  },
+
+ animate: function(start) {
+    if (!this.options.isStopped) {
+        var self  = this,
+          len   = this._latlngs.length,
+          speed = this.options.interval;
+          // this.options.onStart(this._i, this);
+        // Normalize the transition speed from vertex to vertex
+        if (this._i < len && this.i > 0) {
+          speed = this._latlngs[this._i-1].distanceTo(this._latlngs[this._i]) / this.options.distance * this.options.interval;
+        }
+
+        // this.enableTransitions(speed);
+        // Only if CSS3 transitions are supported
+        if (L.DomUtil.TRANSITION) {
+          if (this._icon) { this._icon.style[L.DomUtil.TRANSITION] = ('all ' + speed + 'ms linear'); }
+          if (this._shadow) { this._shadow.style[L.DomUtil.TRANSITION] = 'all ' + speed + 'ms linear'; }
+        }
+
+        // Move to the next vertex
+        this.setLatLng(this._latlngs[this._i]);
+        this._i++;
+        counter = this._i - 1;
+        // Queue up the animation to the next next vertex
+        // if(counter == len - 1) counter = 0;
+        if(this.isStopped) return;
+        this._tid = setTimeout(function(){
+          if (self._i === len) {
+            counter = 0;
+            self.options.onEnd.apply(self, Array.prototype.slice.call(arguments));
+          } else {
+            self.animate();
+          }
+        }, start ? 0 : speed);
+      }
+  },
+   /*
+    Change animation speed function
+  */
+  setSpeed: function(speed){
+    this.options.interval = speed;
+    },
+
+  // Start the animation
+  start: function() {
+    this.options.isStopped = false;
+    if (!this._i) {
+      this._i = resume;
+    }
+    this.animate();
+  },
+  /*
+   Pause The animation.
+   (The start function is called again to continue.)
+  */
+  pause: function () {
+    resume = counter;
+    if (this._tid) {
+      clearTimeout(this._tid);
+      this.options.isStopped = true;
+    }
+  },
+
+  // Stop the animation in place
+  stop: function() {
+    if (this._tid) {
+      clearTimeout(this._tid);
+      this.options.isStopped = true;
+    }
+  },
+
+  setLine: function(latlngs){
+    if (L.DomUtil.TRANSITION) {
+      // No need to to check up the line if we can animate using CSS3
+      this._latlngs = latlngs;
+    } else {
+      // Chunk up the lines into options.distance bits
+      this._latlngs = this._chunk(latlngs);
+      this.options.distance = 10;
+      this.options.interval = 30;
+    }
+    this._i = resume;
+  }
+
+});
+
+L.animatedMarker = function (latlngs, options) {
+  return new L.AnimatedMarker(latlngs, options);
+};
