e78e56f4f23e1c85a5cf073b347e430f878a4e4b
Anguandia <mikeanguandia@gmail.com>
2022-05-07 19:37:54 +0200

    update local branch

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,12 @@ import viewBusReducer from '../features/viewBus/ViewBusSlice';
 import authReducer from '../features/auth/authSlice';
 import routeReducer from '../features/Route/routeSlice';
 import EmployeesReducer from '../features/Employees/EmployeeSlice';
-import authReducer from '../features/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
     viewSearchedBuses: viewBusReducer,
     auth: authReducer,
-    routes: routeReducer
+    routes: routeReducer,
+    employees: EmployeesReducer
   },
 });
--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -63,24 +63,29 @@ export const authSlice = createSlice({
         state.isError = true
         state.message = action.payload
         state.user = null
-        console.log(action.payload)
       })
       .addCase(logout.fulfilled, (state) => {
         state.user = null
       })
-      
+      .addCase(register.pending,(state)=>{
+        state.isLoading = true
+       })
+       .addCase(register.fulfilled,(state,action) =>{
+          state.isLoading= false
+          state.isSuccess = true
+          state.user = action.payload
+       })
+       .addCase(register.rejected, (state,action) =>{
+           state.isLoading = false
+           state.isError= true
+           state.message= action.payload
+           state.user= null
+       })
   },
 })
 
 export const { reset } = authSlice.actions
 
-const initialState = {
-    user: null,
-    isError: false,
-    isSuccess: false,
-    isLoading: false,
-    message: '',
-}
 // Register user
 export const register = createAsyncThunk(
     'auth/register',
@@ -93,35 +98,35 @@ export const register = createAsyncThunk(
         return thunkAPI.rejectWithValue(message)
     }
 })
-export const authSlice= createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-     reset: (state) =>{
-         state.isLoading = false
-         state.isSuccess=false
-         state.isError=false
-         state.message=''
-     }
-    },
-    extraReducers: (builder) => {
-     builder
-     .addCase(register.pending,(state)=>{
-      state.isLoading = true
-     })
-     .addCase(register.fulfilled,(state,action) =>{
-        state.isLoading= false
-        state.isSuccess = true
-        state.user = action.payload
-     })
-     .addCase(register.rejected, (state,action) =>{
-         state.isLoading = false
-         state.isError= true
-         state.message= action.payload
-         state.user= null
-     })
-    }
-})
+// export const authSlice= createSlice({
+//     name: 'auth',
+//     initialState,
+//     reducers: {
+//      reset: (state) =>{
+//          state.isLoading = false
+//          state.isSuccess=false
+//          state.isError=false
+//          state.message=''
+//      }
+//     },
+    // extraReducers: (builder) => {
+    //  builder
+    //  .addCase(register.pending,(state)=>{
+    //   state.isLoading = true
+    //  })
+    //  .addCase(register.fulfilled,(state,action) =>{
+    //     state.isLoading= false
+    //     state.isSuccess = true
+    //     state.user = action.payload
+    //  })
+    //  .addCase(register.rejected, (state,action) =>{
+    //      state.isLoading = false
+    //      state.isError= true
+    //      state.message= action.payload
+    //      state.user= null
+    //  })
+    // }
+// })
 
-export const {reset}= authSlice.actions
+// export const {reset}= authSlice.actions
 export default authSlice.reducer
